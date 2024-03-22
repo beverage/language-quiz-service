7ca446c2f9f33d8adcd5458d5c642eb494f610d0
beverage <alex.beverage@gmail.com>
2024-03-22 18:28:56 +0100

    💪 Added more resiliency to requests
    ChatGTP often returns malformed JSON.  This should help recitify that.

--- src/dbtest/verbs/get.py
+++ src/dbtest/verbs/get.py
@@ -4,6 +4,8 @@ from ..database.verbs import Verb, Reflexivity
 import json
 import logging
 
+from fix_busted_json import repair_json
+
 from openai import OpenAI, ChatCompletion
 
 from sqlalchemy import create_engine, and_
@@ -29,10 +31,30 @@ def generate_reflexivity_prompt(verb_infinitive: str) -> str:
               if the verb can be used both reflexive and non-reflexively return \
               'conditional', otherwise return 'no'."""
 
-def json_format_prompt(verb: str) -> str:
-    return """
-
+def generate_verb_tense_format(verb: str) -> str:
+    return """ \
+                {   \
+                    verb tense (as tense): \
+                    conjugations: [ \
+                        {  \
+                            french pronoun (as 'pronoun'): \
+                            conjugated verb, without its pronoun (as 'verb'):    \
+                            english translation (as 'translation'): \
+                        }  \
+                    ]   \
+                } \
 """
+
+def generate_extra_rules(verb_infinitive: str) -> str:
+    return """Do not return any newlines in the response. \
+            Always use both genders in the 3rd person pronouns.  \
+            Always include 'on' for the 3rd person singular form.  \
+            Replace spaces with _ in the tense names. \
+            Remove all accent marks on the tense names. \
+            The first person pronoun should always be 'je' instead of j' or j. \
+            The pronouns should always be "-" for participles. \
+            All json property names and values need to be enclosed in double quotes. \
+            """
 
 def generate_verb_prompt(verb_infinitive: str):
     #   This prompt still often returns mal-formed JSON, and accent marks where they are not wanted.
@@ -43,23 +65,10 @@ def generate_verb_prompt(verb_infinitive: str):
                 auxiliary: \
                 infinitive: {verb_infinitive} \
                 reflexivity: {generate_reflexivity_prompt(verb_infinitive)} \
-                verb tense (as 'tense'): {{   \
-                    conjugations: [ \
-                        {{  \
-                            french pronoun (as 'pronoun'): \
-                            conjugated verb, without its pronoun (as 'verb'):    \
-                            english translation (as 'translation'): \
-                        }}  \
-                    ]   \
-                }} \
-            Do not return any newlines in the response. \
-            Always use both genders in the 3rd person pronouns.  \
-            Always include 'on' for the 3rd person singular form.  \
-            Replace spaces with _ in the tense names. \
-            Remove all accent marks on the tense names. \
-            The first person pronoun should always be 'je' instead of j' or j. \
-            The pronouns should always be "-" for participles. \
-            All json property names and values need to be enclosed in double quotes. \
+                verb tense (as 'tenses'): [ \
+                    {generate_verb_tense_format(verb_infinitive)} \
+                ] \
+            {generate_extra_rules(verb_infinitive)}
             """
 
 def fetch_verb(requested_verb: str, save_verb: bool) -> str:
@@ -71,20 +80,19 @@ def fetch_verb(requested_verb: str, save_verb: bool) -> str:
         ]
     )
 
-    print(completion.choices[0].message.content)
-
-    #   Attempt to fix mal-formed JSON here.
+    # print(completion.choices[0].message.content)
 
     if save_verb:
 
-        response = completion.choices[0].message.content
-        response_json = json.loads(response)
+        response: str = repair_json(completion.choices[0].message.content) # JSON can often come back mal-formed by ChatGTP.
+        response_json: str = json.loads(response)
 
         infinitive: str = response_json["infinitive"]
 
-        for tense in response_json["tense"]:
+        for response_tense in response_json["tenses"]:
 
-            response_tense = response_json["tense"][tense]
+            tense = response_tense["tense"]
+
             existing_verb: Verb = (
                 session.query(Verb)
                        .filter(Verb.infinitive == requested_verb)
