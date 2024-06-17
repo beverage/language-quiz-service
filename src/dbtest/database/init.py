2825cf8aeafad943b7a31f81b1dfa332ed9edd1d
beverage <alex.beverage@gmail.com>
2024-06-17 15:43:52 +0200

    🪡 Added basic rate limiting to verb initialization
    This won't be used while we are still hardcoding the initial verb set.

--- src/dbtest/database/init.py
+++ src/dbtest/database/init.py
@@ -1,18 +1,21 @@
-from asyncio import ensure_future, gather
+from asyncio import gather, Semaphore
 from ..verbs.get import fetch_verb
 
+# Hardcore some verbs for now.  We will load verb lists later.
 auxiliaries: list[str] = ["avoir", "être"]
 irregulars: list[str] = ["aller", "devoir", "dire", "faire", "pouvoir", "prendre", "savoir", "venir", "voir", "vouloir"]
 
+# Artificial lower bound for testing.  Will make this high enough for the hard coded verbs for now.
+limit = Semaphore(15)
+
+async def rate_limited_verb_fetch(verb: str):
+    async with limit:
+        await fetch_verb(requested_verb=verb)
+
 async def init_auxiliaries(with_common_irregulars=False):
 
-    tasks = []
-
     verbs = auxiliaries + irregulars if with_common_irregulars else auxiliaries
-
-    for verb in verbs:
-        task = ensure_future(fetch_verb(requested_verb=verb))
-        tasks.append(task)
+    tasks = [rate_limited_verb_fetch(verb=verb) for verb in verbs]
         
     await gather(*tasks, return_exceptions=True)
 
