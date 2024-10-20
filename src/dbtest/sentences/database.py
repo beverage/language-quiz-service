a0ead11bd30d00234da9ce2000fc4757c7f4e2c7
beverage <alex.beverage@gmail.com>
2024-10-20 16:33:50 +0200

    💪 Adding sentence fetch queries
    This is a temporary commit for the purpose of reaching out for assistance in better understanding async sqlalchemy and race conditions when generating ORM models.
    
    Probably a million other things are wrong and/or stupid.  That's why I'm asking for help.

--- src/dbtest/sentences/database.py
+++ src/dbtest/sentences/database.py
@@ -1,19 +1,32 @@
 
-from ..database.engine import AsyncSession, get_async_session
-from .models import Pronoun, Sentence, Tense, DirectObject, IndirectPronoun, Negation
+from ..database.engine import AsyncSession, get_async_session, Base
+from .models import Pronoun, Tense, DirectObject, IndirectPronoun, Negation, Sentence
 
-async def get_sentence(verb_infinitive:  str,
+from sqlalchemy import inspect
+from sqlalchemy import select
+
+import logging
+
+async def get_sentence(quantity:         int,
+                       verb_infinitive:  str,
                        pronoun:          Pronoun         = Pronoun.first_person,   # Pronoun and tense will remain both random
                        tense:            Tense           = Tense.present,          # and correct for now.  These values will be
                        direct_object:    DirectObject    = DirectObject.none,      # ignored.
                        indirect_pronoun: IndirectPronoun = IndirectPronoun.none,
                        negation:         Negation        = Negation.none,
-                       is_correct:       bool            = True,                   # This cannot be guaranteed until the AI has responded.
-                       openai_client:    AsyncSession    = get_async_session()):
+                       is_correct:       bool            = True):                   # This cannot be guaranteed until the AI has responded.
 
     async with get_async_session() as session:
-        # The sentence is returned as a DTO.
-        pass
+
+        stmt = select(Sentence).where(Sentence.infinitive == verb_infinitive).limit(quantity)
+
+        logging.info(str(stmt))
+
+        sentence: Sentence = (
+            await session.execute(stmt)
+        )
+
+        return sentence
 
 async def save_sentence(sentence: Sentence):
     async with get_async_session() as session:
