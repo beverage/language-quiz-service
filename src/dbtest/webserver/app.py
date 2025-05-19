4ff17629c08b470830a3c6707e3fb085c1c175d6
beverage <alex.beverage@gmail.com>
2025-05-19 12:42:12 +0200

    ➕ Added simple verb get API call

--- src/dbtest/webserver/app.py
+++ src/dbtest/webserver/app.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from ..sentences.create import create_sentence
+from dbtest.sentences.create import create_sentence
+from dbtest.webserver.verbs.get import get_verb_and_conjugations
 
 app = FastAPI()
 
@@ -11,3 +12,10 @@ async def hello():
 @app.get("/sentence")
 async def sentence():
     return await create_sentence("savoir")
+
+@app.get("/verbs/{infinitive}")
+async def get_verb(infinitive: str):
+    data = await get_verb_and_conjugations(infinitive)
+    if data is None:
+        raise HTTPException(status_code=404, detail="Verb not found")
+    return data
---
+++ src/dbtest/webserver/verbs/get.py
@@ -0,0 +1,52 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from dbtest.database.engine import async_engine
+from dbtest.verbs.models import conjugation_table, verb_table
+
+async_session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_verb_and_conjugations(infinitive: str):
+    async with async_session() as session:
+        result = await session.execute(
+            select(verb_table).where(verb_table.c.infinitive == infinitive)
+        )
+
+        verb_row = result.fetchone()
+        if not verb_row:
+            return None
+
+        verb_id   = verb_row.id
+        auxiliary = verb_row.auxiliary
+
+        result = await session.execute(
+            select(conjugation_table).where(conjugation_table.c.verb_id == verb_id)
+        )
+
+        conjugations = []
+
+        for row in result.fetchall():
+            conjugation = {
+                "tense": row.tense.name,
+                "infinitive": row.infinitive
+            }
+
+            conjugation_fields = [
+                c.name for c in conjugation_table.columns
+                if c.name not in ("id", "verb_id", "tense", "infinitive")
+            ]
+
+            for field in conjugation_fields:
+                value = getattr(row, field)
+
+                if value is not None:
+                    conjugation[field] = value.replace("_", " ")
+
+            conjugations.append(conjugation)
+
+        return {
+            "infinitive": verb_row.infinitive,
+            "auxiliary": auxiliary,
+            "conjugations": conjugations
+        }
